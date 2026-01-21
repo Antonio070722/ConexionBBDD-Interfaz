e807885662489856307a229dce9e9c2cf9a85d5d
PC-CLASE <allopal2911@g.educaand.es>
2026-01-21 16:58:01 +0100

    Cositas en el principal

--- src/main/java/app/Principal.java
+++ src/main/java/app/Principal.java
@@ -1,7 +1,36 @@
 package app;
 
+import conexion.ConexionBBDD;
+
+import java.util.Scanner;
+
 public class Principal {
+
+    static ConexionBBDD conexionBBDD;
+
     static void main() {
+        int op =0 ;
+        Scanner sc = new Scanner(System.in);
+        do {
+        System.out.println("Bienvenido al sistema de AUCORSA.");
+        System.out.println("¿Qué desea hacer?");
+        System.out.println("\t1. Consultar conductores.");
+        System.out.println("\t2. Insertar conductores.");
+        System.out.println("\t3. Salir.");
+
+            op = sc.nextInt();
+            switch (op){
+                case 1:
+                    consultarConductor();
+                    break;
+            }
+
+        }while(op !=0);
+    }
+
+    private static void consultarConductor() {
+
+        conexionBBDD.consultarBBDD("select * from CONDUCTOR");
 
     }
 }
--- src/main/java/conexion/ConexionBBDD.java
+++ src/main/java/conexion/ConexionBBDD.java
@@ -1,19 +1,48 @@
 package conexion;
 
+import com.mysql.cj.jdbc.ConnectionGroup;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ConexionBBDD {
 
     public static final String USER = "root";
     public static final String PASSWORD = "1234";
-    public static final String URL = "jdbc:mysql://localhost:3306/aucorsa";
+    public static final String URL = "jdbc:mysql://127.0.0.1:3306/aucorsa";
 
-    ConexionBBDD() throws SQLException {
+    public ConexionBBDD(){
+    }
 
+    public Connection crearConexionBBDD() throws SQLException {
         Connection con = DriverManager.getConnection(URL, USER, PASSWORD);
+        return con;
+    }
 
+    public void consultarBBDD(String consulta){
+        Connection con = null;
+        try {
+            con = crearConexionBBDD();
+            Statement s = con.createStatement();
+            s.execute(consulta);
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean insertarConductor(String consulta){
+        Connection con = null;
+        try {
+            con = crearConexionBBDD();
+            Statement s = con.createStatement();
+            s.execute(consulta);
+            return true;
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
 }
